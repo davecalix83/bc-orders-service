10424af4c9787ac8b706860a2d7f48e685c9485d
davecalix83 <davecalix@gmail.com>
2024-09-27 16:49:18 -0600

    Refactoring implementation services to reduce code, promote reuse, and separate responsibilities

--- src/main/java/com/cuscatlan/orders/application/service/OrderServiceImpl.java
+++ src/main/java/com/cuscatlan/orders/application/service/OrderServiceImpl.java
@@ -1,27 +1,23 @@
 package com.cuscatlan.orders.application.service;
 
 import com.cuscatlan.orders.application.dto.*;
-import com.cuscatlan.orders.domain.exception.NoOrdersFoundException;
-import com.cuscatlan.orders.domain.exception.OrderDeletionException;
-import com.cuscatlan.orders.domain.exception.OrderNotFoundException;
-import com.cuscatlan.orders.domain.exception.ProductNotFoundException;
-import com.cuscatlan.orders.domain.exception.UpdateOrderException;
+import com.cuscatlan.orders.domain.exception.*;
 import com.cuscatlan.orders.domain.model.Order;
 import com.cuscatlan.orders.domain.model.OrderItem;
-import com.cuscatlan.orders.infrastructure.external.ProductServiceClient;
 import com.cuscatlan.orders.infrastructure.repository.OrderRepository;
 import com.cuscatlan.orders.shared.mapper.OrderItemMapper;
 import com.cuscatlan.orders.shared.mapper.OrderMapper;
 import com.cuscatlan.orders.shared.utils.Common;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -32,25 +28,24 @@ public class OrderServiceImpl implements OrderService {
     private static final String ORDER_CREATION_SUCCESS_MESSAGE = "Order created successfully";
 
     private final OrderRepository orderRepository;
-    private final ProductServiceClient productServiceClient;
+    private final ProductValidationService productValidationService;
     private final OrderMapper orderMapper;
     private final OrderItemMapper orderItemMapper;
-    private final Common common;
+    private final Common utils;
 
     @Override
     public OrderResponseDto createOrder(OrderRequestDto orderRequestDto) {
+
         Order order = orderMapper.toEntity(orderRequestDto);
+
         processOrderItems(order, orderRequestDto.getItems());
+
         order.setStatus("PENDING");
         order.setOrderDate(LocalDateTime.now());
-        order.setTotalAmount(calculateTotalAmount(order.getItems())); // Cambiado
-        Order savedOrder = orderRepository.save(order);
+        order.setTotalAmount(utils.calculateTotalAmount(order.getItems()));
+        Order savedOrder = saveOrder(order);
 
-        return common.buildOrderResponse(
-                savedOrder.getId(),
-                "OK",
-                ORDER_CREATION_SUCCESS_MESSAGE
-        );
+        return buildOrderResponse(savedOrder.getId(), ORDER_CREATION_SUCCESS_MESSAGE);
     }
 
     @Override
@@ -58,9 +53,11 @@ public class OrderServiceImpl implements OrderService {
         List<OrderRequestDto> orders = orderRepository.findAll().stream()
                 .map(orderMapper::toDto)
                 .collect(Collectors.toList());
+
         if (orders.isEmpty()) {
             throw new NoOrdersFoundException(NO_ORDERS_FOUND_MESSAGE);
         }
+
         return orders;
     }
 
@@ -72,39 +69,34 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
-    public Optional<OrderResponseDto> updateOrder(OrderRequestDto orderRequestDto) {
+    public Optional<OrderResponseDto> updateOrder(@NotNull OrderRequestDto orderRequestDto) {
+
         Order existingOrder = orderRepository.findById(orderRequestDto.getId())
                 .orElseThrow(() -> new OrderNotFoundException(orderRequestDto.getId()));
 
-        try {
-            updateOrderDetails(existingOrder, orderRequestDto);
-            processOrderItems(existingOrder, orderRequestDto.getItems());
-            existingOrder.setOrderDate(LocalDateTime.now());
-            existingOrder.setTotalAmount(calculateTotalAmount(existingOrder.getItems()));
-            existingOrder.setStatus(existingOrder.getStatus());
-            Order updatedOrder = orderRepository.save(existingOrder);
-            return Optional.of(
-                    common.buildOrderResponse(updatedOrder.getId(),
-                            "OK",
-                            "Order updated successfully")
-            );
-        } catch (Exception e) {
-            throw new UpdateOrderException("Failed to update order: " + e.getMessage());
-        }
+        updateOrderDetails(existingOrder, orderRequestDto);
+        processOrderItems(existingOrder, orderRequestDto.getItems());
+
+        existingOrder.setOrderDate(LocalDateTime.now());
+        existingOrder.setTotalAmount(utils.calculateTotalAmount(existingOrder.getItems()));
+        existingOrder.setStatus(existingOrder.getStatus());
+        Order updatedOrder = saveOrder(existingOrder);
+
+        return Optional.of(buildOrderResponse(updatedOrder.getId(), "Order updated successfully"));
     }
 
     @Override
     public OrderResponseDto deleteOrder(Long id) {
+
         if (!orderRepository.existsById(id)) {
             throw new OrderNotFoundException(id);
         }
+
         try {
+
             orderRepository.deleteById(id);
-            return common.buildOrderResponse(
-                    id,
-                    "OK",
-                    "Order deleted successfully"
-            );
+            return utils.buildOrderResponse(id, "OK", "Order deleted successfully");
+
         } catch (Exception e) {
             throw new OrderDeletionException("Failed to delete order with ID: " + id + ". Reason: " + e.getMessage());
         }
@@ -112,21 +104,17 @@ public class OrderServiceImpl implements OrderService {
 
     private void processOrderItems(@NotNull Order order, @NotNull List<OrderItemDto> orderItemDtos) {
         order.getItems().clear();
+
         orderItemDtos.forEach(orderItemDto -> {
-            ProductDto product = validateProduct(orderItemDto.getProductId());
+
+            ProductDto product = productValidationService.validateProduct(orderItemDto.getProductId());
+
             OrderItem orderItem = orderItemMapper.toEntity(orderItemDto);
             orderItem.setPrice(product.getPrice());
             orderItem.setOrder(order);
+
             order.getItems().add(orderItem);
         });
-    }
-
-    private ProductDto validateProduct(Long productId) {
-        ProductDto product = productServiceClient.getProductById(productId);
-        if (product == null) {
-            throw new ProductNotFoundException(productId);
-        }
-        return product;
     }
 
     private void updateOrderDetails(@NotNull Order existingOrder, @NotNull OrderRequestDto orderRequestDto) {
@@ -136,10 +124,11 @@ public class OrderServiceImpl implements OrderService {
         existingOrder.setTotalAmount(orderRequestDto.getTotalAmount());
     }
 
-  
-    private BigDecimal calculateTotalAmount(List<OrderItem> items) {
-        return items.stream()
-                .map(item -> item.getPrice().multiply(BigDecimal.valueOf(item.getQuantity()))) 
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    private @NotNull Order saveOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    private OrderResponseDto buildOrderResponse(Long orderId, String message) {
+        return utils.buildOrderResponse(orderId, "OK", message);
     }
 }
--- src/main/java/com/cuscatlan/orders/shared/utils/Common.java
+++ src/main/java/com/cuscatlan/orders/shared/utils/Common.java
@@ -1,12 +1,17 @@
 package com.cuscatlan.orders.shared.utils;
 
 import com.cuscatlan.orders.application.dto.OrderResponseDto;
+import com.cuscatlan.orders.domain.model.OrderItem;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class Common {
+
     public OrderResponseDto buildOrderResponse(Long orderId, String status, String message) {
         return OrderResponseDto.builder()
                 .orderId(orderId)
@@ -14,5 +19,11 @@ public class Common {
                 .message(message)
                 .processedAt(LocalDateTime.now())
                 .build();
+    }
+
+    public BigDecimal calculateTotalAmount(@NotNull List<OrderItem> items) {
+        return items.stream()
+                .map(item -> item.getPrice().multiply(BigDecimal.valueOf(item.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
